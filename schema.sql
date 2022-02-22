d69e51d837e44a3cebd8a77c7b5297037df3fc88
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-02-22 23:24:46 +0100

    Create two new tables and insert the values

--- schema.sql
+++ schema.sql
@@ -12,3 +12,21 @@ CREATE TABLE animals (
 -- Phase 2 update animals table
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+-- Phase 3 query multiple tables
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
